e71f601a4c77cb932f5d0b78083e7602919609dc
Carlos A. Arguelles <carguelles@fas.harvard.edu>
2021-12-08 12:35:12 -0500

    fixed runscript.py for python3

--- atlas-outreach-data-tools-framework-1.1/RunScript.py
+++ atlas-outreach-data-tools-framework-1.1/RunScript.py
@@ -17,7 +17,7 @@ def buildProcessingDict(configuration, samples):
         try:
             processingDict[sample] = configuration.Processes[sample]
         except :
-            print "Name of Sample %s not recognized. Sample was not added to processing list!" % sample
+            print("Name of Sample %s not recognized. Sample was not added to processing list!" % sample)
     return processingDict
 
 def checkAnalysis(configuration, analysisOption):
@@ -26,7 +26,7 @@ def checkAnalysis(configuration, analysisOption):
         importedAnalysisModule = importlib.import_module("Analysis." + analysisName)
         configuration.Job["Analysis"] = analysisName
     except ImportError:
-        print "Error when trying to read the analysis code for %s. Please check name validity" % analysisName
+        print("Error when trying to read the analysis code for %s. Please check name validity" % analysisName)
         sys.exit(1)
 
 def BuildJob(configuration, processName, fileLocation):
